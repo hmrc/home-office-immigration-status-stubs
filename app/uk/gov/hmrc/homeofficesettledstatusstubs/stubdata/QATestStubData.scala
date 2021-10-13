88c1871bd6d97b49c57fc00cbfeebb8852f7a1a9
Kerry-anne Kane <kerry-anne.kane@opencastsoftware.com>
2021-10-13 11:42:54 +0100

    added QA data

---
+++ app/uk/gov/hmrc/homeofficesettledstatusstubs/stubdata/QATestStubData.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.homeofficesettledstatusstubs.stubdata
+
+import uk.gov.hmrc.homeofficesettledstatusstubs.models._
+import uk.gov.hmrc.homeofficesettledstatusstubs.stubdata.DemoStubData.formatter
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+object QATestStubData {
+
+  val formatter = DateTimeFormatter.ofPattern("d/MM/yyyy")
+
+  val johnSmith = StatusCheckResult(
+    "John Smith",
+    LocalDate.parse("15/10/1971", formatter),
+    "ESP",
+    List(
+      ImmigrationStatus(
+        statusStartDate = LocalDate.parse("05/10/2021", formatter),
+        statusEndDate = Some(LocalDate.parse("05/02/2022", formatter)),
+        productType = "EUS",
+        immigrationStatus = "LTR",
+        noRecourseToPublicFunds = false
+      )
+    )
+  )
+
+  val results: Map[String, StatusCheckResult] = Map(
+    "SP317690D" -> johnSmith
+  )
+}
